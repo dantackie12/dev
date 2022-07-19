accb42ee122fd4a08f8bece04925d633bc55f2e7
root <dantackie12@gmail.com>
2022-07-19 03:51:30 +0000

    First trial

---
+++ Readme.md
@@ -0,0 +1,5 @@
+this is a read me file
+
+
+The script in this repo is to create users snd do other tasks
+You can run this using "bash script.sh" or "./script.sh"
---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "welcone to scripting lifestyle"
+echo
+echo
+sleep 3
+echho "if you like I can create a user with a folder for you"
+sleep 2
+echo
+echo "would you like me to go ahead and create a personal user for you?"
+
