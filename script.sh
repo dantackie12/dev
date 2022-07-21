4017a6c705098309e424b0375727b6a6f48ae447
root <dantackie12@gmail.com>
2022-07-21 04:28:54 +0000

    New Changes

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ sleep 2
 echo
 echo
 echo "would you like me to go ahead and create a personal user for you?"
-read ans
+#read ans
 #write an if statement if the answer is yes continue, if the answer is no quit
 echo "Thanks for using my services"
 echo
@@ -21,9 +21,10 @@ echo "2. Create a group"
 echo "3. Create a file"
 echo "4. Create a folder"
 echo "5. Create a webserver"
-read option
+#read option
 sleep 2
 echo "You have selected option $option "
 echo "I will go ahead and follow your wishes"
 echo
 #write a case state that will perform any action that the user picks
+echo "Done!!"
